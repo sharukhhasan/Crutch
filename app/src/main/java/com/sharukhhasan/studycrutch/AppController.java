2438152dd736be63dcc58f790f06eb24f20656d2
Sharukh Hasan <sharukhhasan@Sharukhs-MacBook-Pro.local>
2016-05-24 19:54:03 -0500

    added engr course file & controller

--- app/src/main/java/com/sharukhhasan/studycrutch/AppController.java
+++ app/src/main/java/com/sharukhhasan/studycrutch/AppController.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,18 +22,73 @@ public class AppController extends Application{
     public static Map<String, ArrayList<String>> courseMap;
     public static ArrayList<String> courseList;
     private static boolean num = false;
+    private static final String[] csvEngrFiles = {"BME.csv", "CBE.csv", "CEE.csv", "ECE.csv", "ENGR.csv", "IE.csv", "ME.csv"};
+    public static Map<String, ArrayList<String>> engrCourseMap;
+    public static ArrayList<String> engrDepts;
 
     @Override
     public void onCreate()
     {
         super.onCreate();
 
-        try {
-            courseNumberMap = getCourses(numberPath);
-            courseNameMap = getCourses(namePath);
-            syncData(courseNumberMap, courseNameMap);
-        } catch (IOException e) {
-            e.printStackTrace();
+        for(String file : csvEngrFiles)
+        {
+            try {
+                buildCourseList(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void createEngrCourseMao() throws IOException
+    {
+        engrCourseMap = new HashMap<>();
+        engrDepts = new ArrayList<>();
+        boolean firstInput = true;
+
+        for(String engrFile : csvEngrFiles)
+        {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(engrFile));
+            String lineInput = null;
+            firstInput = true;
+            String dept = null;
+
+            while((lineInput = bufferedReader.readLine()) != null)
+            {
+                Scanner sc = new Scanner(lineInput);
+                sc.useDelimiter(",");
+
+                if(firstInput)
+                {
+                    dept = sc.next();
+                    engrDepts.add(dept);
+                    engrCourseMap.put(dept, new ArrayList<String>());
+                    firstInput = false;
+                }
+                else
+                {
+                    engrCourseMap.get(dept).add(sc.next() + " " + sc.next());
+                }
+            }
+        }
+
+    }
+
+    public static void buildCourseList(String path) throws IOException
+    {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        String lineInput = null;
+
+        while((lineInput = bufferedReader.readLine()) != null)
+        {
+            Scanner sc = new Scanner(lineInput);
+            sc.useDelimiter(",");
+
+            while(sc.hasNext())
+            {
+                System.out.println(sc.next());
+            }
         }
     }
 
