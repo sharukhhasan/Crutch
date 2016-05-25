43c32cf4c6c68019612bf1342cf53641a26582fd
Sharukh Hasan <sharukhhasan@Sharukhs-MacBook-Pro.local>
2016-05-24 21:05:45 -0500

    adding searchable spinner

--- app/src/main/java/com/sharukhhasan/studycrutch/AppController.java
+++ app/src/main/java/com/sharukhhasan/studycrutch/AppController.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -31,13 +30,10 @@ public class AppController extends Application{
     {
         super.onCreate();
 
-        for(String file : csvEngrFiles)
-        {
-            try {
-                buildCourseList(file);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try {
+            createEngrCourseMao();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -75,76 +71,13 @@ public class AppController extends Application{
 
     }
 
-    public static void buildCourseList(String path) throws IOException
+    public ArrayList<String> getEngrDepts()
     {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-        String lineInput = null;
-
-        while((lineInput = bufferedReader.readLine()) != null)
-        {
-            Scanner sc = new Scanner(lineInput);
-            sc.useDelimiter(",");
-
-            while(sc.hasNext())
-            {
-                System.out.println(sc.next());
-            }
-        }
-    }
-
-    public Map<String, ArrayList<String>> getCourses(String path) throws IOException {
-        courseMap = new HashMap<>();
-        BufferedReader reader = new BufferedReader(new FileReader(path));
-        String inputLine;
-
-        while((inputLine = reader.readLine()) != null)
-        {
-            Scanner lineScanner = new Scanner(inputLine);
-            lineScanner.useDelimiter(",");
-            String prev_dept = null;
-
-            while(lineScanner.hasNext())
-            {
-                String temp = lineScanner.next();
-                if(!num)
-                {
-                    prev_dept = temp;
-                    courseMap.put(temp, new ArrayList<String>());
-                    num = true;
-                }
-                else
-                {
-                    courseMap.get(prev_dept).add(temp);
-                }
-            }
-            num = false;
-        }
-
-        return courseMap;
-    }
-
-    public void syncData(Map<String, ArrayList<String>> numberMap, Map<String, ArrayList<String>> nameMap)
-    {
-        courseMap = new HashMap<>();
-        courseList = new ArrayList<>();
-        for(Map.Entry<String, ArrayList<String>> entry : numberMap.entrySet())
-        {
-            String dept = entry.getKey();
-            ArrayList<String> numbers = entry.getValue();
-            ArrayList<String> names = nameMap.get(dept);
-            courseMap.put(dept, new ArrayList<String>());
-
-            for(int i = 0; i < numbers.size(); i++)
-            {
-                String concatCourse = dept + numbers.get(i) + " " + names.get(i);
-                courseMap.get(dept).add(concatCourse);
-                courseList.add(concatCourse);
-            }
-        }
+        return engrDepts;
     }
 
     public Map<String, ArrayList<String>> getCourses()
     {
-        return courseMap;
+        return engrCourseMap;
     }
 }
--- app/src/main/java/com/sharukhhasan/studycrutch/activities/CourseInputActivity.java
+++ app/src/main/java/com/sharukhhasan/studycrutch/activities/CourseInputActivity.java
@@ -20,9 +20,11 @@ import java.util.Map;
 public class CourseInputActivity extends AppCompatActivity {
     public static final String TAG = "CourseInputActivity";
     private ImageButton btnAdd;
+    private Spinner depts;
     private Spinner courses;
-    private Map<String, ArrayList<String>> coursesMap = AppController.courseMap;
-    private ArrayList<String> coursesList = AppController.courseList;
+    private Map<String, ArrayList<String>> coursesMap;
+    private ArrayList<String> deptList;
+    private AppController controller = new AppController();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,22 +32,17 @@ public class CourseInputActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_input);
 
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                LinearLayout newLinear = new LinearLayout(CourseInputActivity.this);
-                Spinner newSpinner = new Spinner(CourseInputActivity.this);
+        coursesMap = controller.getCourses();
+        deptList = controller.getEngrDepts();
+        String[] deptArray = deptList.toArray(new String[deptList.size()]);
+        ArrayAdapter<CharSequence> deptAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, deptArray);
 
-                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(CourseInputActivity.this, R.array.coursesArray, android.R.layout.simple_spinner_item);
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        depts = (Spinner) findViewById(R.id.departmentSpinner);
+        depts.setAdapter(deptAdapter);
+
+        courses = (Spinner) findViewById(R.id.courseSpinner);
 
 
-                newSpinner.setAdapter(adapter);
-                newLinear.addView(newSpinner);
-                setContentView(newLinear);
-            }
-        });
     }
 
 
