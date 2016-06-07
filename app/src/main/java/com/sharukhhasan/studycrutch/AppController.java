41bf983d641ab80092c7794f2af855258173bde5
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2016-06-07 02:08:12 -0500

    removed twitter ref

--- app/src/main/java/com/sharukhhasan/studycrutch/AppController.java
+++ app/src/main/java/com/sharukhhasan/studycrutch/AppController.java
@@ -1,17 +1,12 @@
 package com.sharukhhasan.studycrutch;
 
 import android.app.Application;
-import android.os.Bundle;
 
 import com.sharukhhasan.studycrutch.models.Course;
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import io.fabric.sdk.android.Fabric;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -19,11 +14,6 @@ import java.util.Scanner;
  * Created by sharukhhasan on 5/20/16.
  */
 public class AppController extends Application{
-
-    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "BZiwEDSzT5K367WqCmD1WO6RD";
-    private static final String TWITTER_SECRET = "oC2YvyXcIukWZMSYTolmNixjJwacjJpxUFvKnkeSvv7AM0yQ7b";
-
     public static final String FIREBASE_URL = "https://studycrutch.firebaseio.com/";
     public static ArrayList<String> engrDepts = getEngrDepts();
     public static Map<String, ArrayList<String>> engrCourseMap = getEngrCourseMap();
@@ -34,8 +24,6 @@ public class AppController extends Application{
     public void onCreate()
     {
         super.onCreate();
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
 
         try {
             getDeptList();
@@ -91,5 +79,10 @@ public class AppController extends Application{
     public static ArrayList<String> getEngrDepts()
     {
         return engrDepts;
+    }
+
+    public static void addCourse(Course course)
+    {
+        enrolledCourses.add(course);
     }
 }
